7c97f108518d932847a2ed82d2a4dc5ca6db5b36
juanes <juanesgym2018@gmail.com>
2020-09-24 11:20:26 -0500

    javaScript

---
+++ alfabeto.js
@@ -0,0 +1,9 @@
+function capturar1(){
+    var capturar1=document.getElementById("alfabeto1").value;
+    console.log(capturar1)
+}
+
+function capturar2(){
+    var capturar2=document.getElementById("alfabeto2").value;
+    console.log(capturar2)
+}
