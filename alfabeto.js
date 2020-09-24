e26d702147e9da92b67937889b20b5771f17318b
juanes <juanesgym2018@gmail.com>
2020-09-24 13:31:28 -0500

    agregando el collapse

--- alfabeto.js
+++ alfabeto.js
@@ -1,9 +1,34 @@
 function capturar1(){
-    var capturar1=document.getElementById("alfabeto1").value;
-    console.log(capturar1)
+ 
+    
+     capturarele=document.getElementById("alfabeto1").value;
+     
+    if(capturarele.length==1){
+        console.log(capturarele)
+        agregar1();
+    }else{
+        alert("tiene que digitar solo un simbolo");
+    }
 }
 
 function capturar2(){
-    var capturar2=document.getElementById("alfabeto2").value;
-    console.log(capturar2)
+    capturarele2=document.getElementById("alfabeto2").value;
+    if(capturarele2.length==1){
+        console.log(capturarele2)
+        agregar2();
+    }else{
+        alert("tiene que digitar solo un simbolo");
+    }
+}
+
+var alf1= [];
+ function agregar1(){
+     alf1.push(capturarele);
+    console.log(alf1);
+}
+
+var alf2 = [];
+function agregar2(){
+    alf2.push(capturarele2);
+   console.log(alf2);
 }
