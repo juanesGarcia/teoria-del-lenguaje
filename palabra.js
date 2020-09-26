525b482ed04ac7bd1841a38f414e0eac7405aace
juanes <juanesgym2018@gmail.com>
2020-09-26 14:34:05 -0500

    completar palabra

--- palabra.js
+++ palabra.js
@@ -1,4 +1,19 @@
 function capturarpalabra(){
     capturapa=document.getElementById("palabra").value;
     console.log(capturapa);
+    console.log(capturapa.length);
+}
+
+function cardinalidad() {
+    document.getElementById("p1").innerHTML= capturapa.length;
+}
+
+function inversa(){
+    var x=capturapa.length;
+    var cadenaInvertida=" ";
+    while (x>=0) {
+        cadenaInvertida = cadenaInvertida + capturapa.charAt(x);
+        x--;
+      }
+      document.getElementById("p2").innerHTML= cadenaInvertida;
 }
